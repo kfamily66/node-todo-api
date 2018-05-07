4ed0c31cfa4370506fc53beffebf7f8c7c1203da
unknown <kfamily66@yandex.ru>
2018-05-07 19:42:43 +0500

    Test GET /todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -8,9 +8,21 @@ const {
     Todo
 } = require('./../models/todo');
 
+
+const todos = [{
+    text: 'First todo'
+}, {
+    text: 'Second todo'
+}];
+
+
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
-})
+    Todo.remove({}).then(() => {
+        Todo.insertMany(todos);
+        done();
+    });
+});
 
 describe('POST /todos:', () => {
     it('should create new todo', (done) => {
@@ -30,7 +42,9 @@ describe('POST /todos:', () => {
                     return done(err);
                 }
 
-                Todo.find().then((todos) => {
+                Todo.find({
+                    text
+                }).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -53,9 +67,22 @@ describe('POST /todos:', () => {
                 }
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => done(e))
             })
     })
-})
+});
+
+describe('GET /todos:', () => {
+
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
+    });
+});
