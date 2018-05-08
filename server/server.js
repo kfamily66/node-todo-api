6cbd6f900b4b28f3ff9c2ca51093f090b470564f
unknown <kfamily66@yandex.ru>
2018-05-08 12:46:06 +0500

    Changed port for heroku

--- server/server.js
+++ server/server.js
@@ -61,8 +61,8 @@ app.get('/todos/:id', (req, res) => {
 
 })
 
-app.listen(3000, () => {
-    console.log('Started on port 3000...');
+app.listen(port, () => {
+    console.log(`Started on port ${port}...`);
 
 });
 
