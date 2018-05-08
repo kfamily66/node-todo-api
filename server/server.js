806c943cae33739cf15ce363cfb24f2462b8f074
unknown <kfamily66@yandex.ru>
2018-05-08 19:36:47 +0500

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -59,7 +59,27 @@ app.get('/todos/:id', (req, res) => {
         res.status(400).send();
     })
 
+});
+
+app.delete('/todos/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (!ObjectId.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send(todo);
+
+    }).catch((err) => {
+        res.status(400).send();
+    })
 })
+
+
 
 app.listen(port, () => {
     console.log(`Started on port ${port}...`);
