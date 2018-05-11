4b1688ebacb3e67ed502f36fbcd6c1fef43ece4d
unknown <kfamily66@yandex.ru>
2018-05-11 18:29:39 +0500

    Add GET /users/me route and authenticate middleware

--- server/models/user.js
+++ server/models/user.js
@@ -61,6 +61,25 @@ UserSchema.methods.generateAuthToken = function () {
     })
 }
 
+UserSchema.statics.findByToken = function (token) {
+    var User = this;
+    var decoded;
+
+    try {
+        decoded = jwt.verify(token, 'secret_word');
+    } catch (e) {
+        return Promise.reject();
+    }
+
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.access': 'auth',
+        'tokens.token': token
+    });
+
+}
+
+
 const User = mongoose.model('User', UserSchema);
 
 module.exports = {
--- server/server.js
+++ server/server.js
@@ -16,6 +16,9 @@ const {
 const {
     User
 } = require('./models/user');
+const {
+    authenticate
+} = require('./middleware/authenticate');
 
 
 const port = process.env.PORT;
@@ -129,8 +132,14 @@ app.post('/users', (req, res) => {
     }).catch((err) => {
         res.status(400).send(err);
     })
-})
+});
 
+
+app.get('/users/me', authenticate, (req, res) => {
+
+    res.send(req.user);
+
+});
 
 
 app.listen(port, () => {
